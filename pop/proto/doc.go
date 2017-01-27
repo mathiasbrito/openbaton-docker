05c16e5f36128ba1f0568e9b3dabc153402023bf
Marco Cilloni <m.c.cilloni@gmail.com>
2017-01-28 00:25:54 +0100

    More comments

---
+++ pop/proto/doc.go
@@ -0,0 +1,3 @@
+// Package proto defines the Pop service, a gRPC based protocol to handle and control a Docker Point-of-Presence.
+// This package contains the protobuf-defined interfaces of the protocol, plus a few constants.
+package proto
