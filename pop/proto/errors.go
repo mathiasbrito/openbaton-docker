063f8fc48784d5825886729086a25851b2a8a31c
Marco Cilloni <m.c.cilloni@gmail.com>
2017-01-28 00:25:39 +0100

    More comments

--- pop/proto/errors.go
+++ pop/proto/errors.go
@@ -6,9 +6,21 @@ import (
 )
 
 var (
+	// AuthErr represent an authentication failure (ie, wrong password).
 	AuthErr         = grpc.Errorf(codes.InvalidArgument, "invalid credentials")
+
+	// InternalErr represents an internal crash of the server.
 	InternalErr     = grpc.Errorf(codes.Internal, "server fault")
+
+	// InvalidArgErr signals the caller that the arguments given with the request
+	// are invalid.
 	InvalidArgErr   = grpc.Errorf(codes.InvalidArgument, "invalid arguments")
+
+	// InvalidTokenErr signals the caller that its token is invalid, and a new
+	// session should be started through an invocation of Login().
 	InvalidTokenErr = grpc.Errorf(codes.PermissionDenied, "invalid token")
+
+	// NotLoggedErr means that the caller tried to execute any operation different 
+	// from Login without a valid token.
 	NotLoggedErr    = grpc.Errorf(codes.Unauthenticated, "not authenticated")
 )
